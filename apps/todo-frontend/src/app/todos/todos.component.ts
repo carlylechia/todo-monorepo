4538ec6daf5688b02ebc31cf16f27856c0efdd55
carlylechia <chiacarlyle@gmail.com>
2024-01-27 19:35:30 +0100

    Use httpclient to fetch data from express endpoints and display a list of todos in the ui

--- apps/todo-frontend/src/app/todos/todos.component.ts
+++ apps/todo-frontend/src/app/todos/todos.component.ts
@@ -1,13 +1,46 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
-export class TodosComponent {
+export class TodosComponent  implements OnInit {
+  todos: Todo[] = [];
+  selectedTodo: Todo | null = null;
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos(): void {
+    this.todoService.getTodos().subscribe((todos: Todo[]) => {
+      this.todos = todos;
+    });
+  }
+
+  updateTodoStatus(todo: Todo): void {
+    todo._id && this.todoService.updateTodo(todo._id, todo).subscribe(() => {
+      this.loadTodos();
+    });
+  }
+
+  deleteTodo(id: string): void {
+    this.todoService.deleteTodo(id).subscribe(() => {
+      this.loadTodos();
+    });
+  }
+
+  showTodoDetails(todo: Todo): void {
+    this.selectedTodo = todo;
+  }
 }
